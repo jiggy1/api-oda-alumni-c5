ac903d460866d0c43cb1ad465d8e4cafdd00e41e
Ange <angepatrickgoprou@gmail.com>
2023-01-16 19:37:14 +0000

    edit projet

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const cors = require('cors')
-const participantsRoutes = require('./routes/participants')
+const projetsRoutes = require('./routes/participants')
 
 const app = express()
 
 app.use(express.json())
 app.use(cors())
 
-app.use('/api/participants', participantsRoutes)
+app.use('/api/projets', projetsRoutes)
 
 module.exports = app
